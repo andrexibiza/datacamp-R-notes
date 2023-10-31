7385d8b49308017273be9acbb6f63f9911990edf
andrexibiza <andrexibiza@icloud.com>
2023-10-30 23:21:45 -0500

    Update TermProject_v0.5.sql

--- TermProject_v0.5.sql
+++ TermProject_v0.5.sql
@@ -2,7 +2,7 @@ SET SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES,ONLY_FULL_GROUP_BY';
 SET GLOBAL max_allowed_packet = 1073741824;
 SELECT @@max_allowed_packet;
 
-CREATE SCHEMA IF NOT EXISTS termproject;
+CREATE SCHEMA IF NOT EXISTS termproject;CategoryID
 USE termproject;
 -- Create the sales table if it doesn't exist
 CREATE TABLE IF NOT EXISTS sales (
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS customers(
     FirstName VARCHAR(45),
     MiddleInitial VARCHAR(1),
     LastName VARCHAR(45),
-    CityID INT REFERENCES cities (CityID),
+    CityID INT REFERENCES cities (CityID) ON DELETE RESTRICT,
     Address VARCHAR(90));
 CREATE TABLE IF NOT EXISTS employees(
 	EmployeeID INT PRIMARY KEY,
@@ -41,8 +41,25 @@ CREATE TABLE IF NOT EXISTS employees(
     LastName VARCHAR(45),
     BirthDate DATE,
     Gender VARCHAR(1),
-    CityID INT REFERENCES Cities (CityID),
+    CityID INT REFERENCES Cities (CityID) ON DELETE RESTRICT,
     HireDate DATE);
 CREATE TABLE IF NOT EXISTS products(
-)
+	ProductID INT PRIMARY KEY,
+	ProductName VARCHAR(45),
+    Price NUMERIC,
+    CategoryID INT REFERENCES categories (category_id) ON DELETE RESTRICT,
+    Class VARCHAR(45),
+    ModifyDate DATE,
+    Resistant VARCHAR(45),
+    IsAllergic CHAR(5) CHECK (IsAllergic IN ('True','False')),
+    VitalityDays NUMERIC(3));
 CREATE TABLE IF NOT EXISTS sales(
+	SalesID INT PRIMARY KEY,
+    SalesPersonID INT REFERENCES employees (EmployeeID) ON DELETE RESTRICT,
+    CustomerID INT REFERENCES customers (CustomerID) ON DELETE RESTRICT,
+    ProductID INT REFERENCES products (ProductID) ON DELETE RESTRICT,
+    Quantity INT,
+    Discount NUMERIC,
+    TotalPrice NUMERIC,
+    SalesDate DATE,
+    TransactionNumber VARCHAR(90));
