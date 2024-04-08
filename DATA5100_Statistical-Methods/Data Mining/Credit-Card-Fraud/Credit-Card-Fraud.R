ebd75a144c66b5a484d92707fce4d99484465e65
andrexibiza <andrexibiza@icloud.com>
2024-04-07 22:16:49 -0500

    commit1

---
+++ DATA5100_Statistical-Methods/Data Mining/Credit-Card-Fraud/Credit-Card-Fraud.R
@@ -0,0 +1,24 @@
+# Load packages
+library(tidyverse)
+library(naivebayes)
+library(psych)
+
+# Load credit card fraud data
+df <- read.csv("credit_card_fraud.csv")
+str(df)
+
+xtabs(~is_fraud+amt, data = df)
+
+# Factor variables
+df$is_fraud <- as.factor(df$is_fraud)
+df$merchant <- as.factor(df$merchant)
+df$category <- as.factor(df$category)
+df$state <- as.factor(df$state)
+df$city <- as.factor(df$city)
+str(df)
+
+# Partition data
+set.seed(666)
+ind <- sample(2, nrow(df), replace = TRUE, prob = c(0.8, 0.2))
+train <- df[ind == 1, ]
+test <- df[ind == 2, ]
